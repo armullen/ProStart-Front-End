485ce3b79294f599ef726b2b6c20ba836b0bb6f2
armullen <amber.r.mullen@gmail.com>
2023-07-08 16:48:36 -0500

    done for the day

--- prostart/src/components/Banner.js
+++ prostart/src/components/Banner.js
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom'
 function Banner() {
     return(
         <div className="banner">
-            <Link to='/login'>Log In</Link>
-            <Link to='/signup'>Sign Up</Link>
+            <div className='bannerlinks'>
+                <Link to='/login'>Log In</Link>
+                <Link to='/signup'>Sign Up</Link>
+            </div>
         </div>
     )
 }
--- prostart/src/components/Footer.js
+++ prostart/src/components/Footer.js
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom'
 function Footer () {
     return (
         <div className='socials'> 
-            <Link to='/contactus'>Connect with us!</Link>           
+            <Link to='/contactus'>Connect with us!</Link>
+            <h4>Copyright 2023 ProStart</h4>
+            <h4>Need a website?</h4>           
         </div> 
     )
 }
