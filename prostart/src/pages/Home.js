ef1cdb08cac81765244b7c94a704b4496d3c2093
armullen <amber.r.mullen@gmail.com>
2023-07-09 18:35:35 -0500

    break time

--- prostart/src/pages/Home.js
+++ prostart/src/pages/Home.js
@@ -7,15 +7,29 @@ function Home () {
     return(
         <Container>
             <Row>
+                <Col>
                 <Card>
                     <Card.Body>
-                    <Card.Text>Our Mission
+                    <Card.Text>Where players go to get better.
                     </Card.Text>
+                        </Card.Body>
+                             <Card.Img variant="bottom" src="https://scontent-msp1-1.xx.fbcdn.net/v/t39.30808-6/331317465_564677602055602_3284773390598219659_n.jpg?stp=cp6_dst-jpg_s960x960&_nc_cat=100&ccb=1-7&_nc_sid=8631f5&_nc_ohc=XWKjIbvdiDoAX8IU6Y1&_nc_ht=scontent-msp1-1.xx&oh=00_AfDyg5ajqV8HQZgPkhVI5rfur2Bra1sRDyfrauR5Cp7I-w&oe=64B0CE9E" />
+                    </Card>
+                </Col>
+                <Col>
+                <Card>
+                    <Card.Header>Our Mission Statement:</Card.Header>
+                    <Card.Body>
+                        <blockquote className="blockquote mb-0">
+                            <p>{' '}
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                                posuere erat a ante.{' '}
+                            </p>
+                        </blockquote>
                     </Card.Body>
-                         <Card.Img variant="bottom" src="https://scontent-msp1-1.xx.fbcdn.net/v/t39.30808-6/331317465_564677602055602_3284773390598219659_n.jpg?stp=cp6_dst-jpg_s960x960&_nc_cat=100&ccb=1-7&_nc_sid=8631f5&_nc_ohc=XWKjIbvdiDoAX8IU6Y1&_nc_ht=scontent-msp1-1.xx&oh=00_AfDyg5ajqV8HQZgPkhVI5rfur2Bra1sRDyfrauR5Cp7I-w&oe=64B0CE9E" />
                 </Card>
+                </Col>
             </Row> 
-
             <Row>
                 <Col>SUBSCRIBE TO OUR NEWSLETTER</Col>
                 <Col>UPCOMING EVENTS</Col>
